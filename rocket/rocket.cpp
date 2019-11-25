fec854513e5a0f5e0d6534becee90cff7ab52a5d
Anders Sundman (asum) <anders.sundman@tobii.com>
2019-11-25 14:25:43 +0100

    Implement feature to make test pass
    We add a state variable to keep track on if we have launched or
    not. This will make the test pass, so we can remove the wip tag.

--- rocket/rocket.cpp
+++ rocket/rocket.cpp
@@ -1,5 +1,9 @@
 #include "rocket.hpp"
 #include <iostream>
 
-void Rocket::launch() { std::cout << "ROCKET LAUNCHING!" << std::endl; }
-bool Rocket::is_launched() const { return false; }
+void Rocket::launch() {
+    std::cout << "ROCKET LAUNCHING!" << std::endl;
+    this->_has_launched = true;
+}
+
+bool Rocket::is_launched() const { return this->_has_launched; }
--- rocket/rocket.hpp
+++ rocket/rocket.hpp
@@ -1,5 +1,9 @@
 class Rocket {
 public:
+  Rocket() : _has_launched(false) {}
   void launch();
   bool is_launched() const;
+
+private:
+  bool _has_launched;
 };
